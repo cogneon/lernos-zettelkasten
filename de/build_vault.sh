1ca04ced1371ce196f145934dc0ff79220498255
saertna <83655354+saertna@users.noreply.github.com>
2023-12-16 20:21:37 +0100

    Path correction to find files

--- de/build_vault.sh
+++ de/build_vault.sh
@@ -1,22 +1,22 @@
 ## update the Vault
 # delete the old files in vault first
-rm -rf LernOS-Zettelkasten/Lernpfad/*.md
+rm -rf ./de/LernOS-Zettelkasten/Lernpfad/*.md
 
 # Delete content from the images folder
-rm -rf LernOS-Zettelkasten/Lernpfad/images/*
+rm -rf ./de/LernOS-Zettelkasten/Lernpfad/images/*
 
 # Copy .md files to the Lernpfad folder
-cp -r src/*.md LernOS-Zettelkasten/Lernpfad
+cp -r ./de/src/*.md LernOS-Zettelkasten/Lernpfad
 
 # Remove the script block from the .md files
-for file in LernOS-Zettelkasten/Lernpfad/*.md; do
+for file in ./de/LernOS-Zettelkasten/Lernpfad/*.md; do
   sed -i 's/<script.*?>//g' "$file"
 done
 
 # Copy non-.svg images to the Lernpfad/images folder
-cp -r src/images/*.jpg LernOS-Zettelkasten/Lernpfad/images
-cp -r src/images/*.png LernOS-Zettelkasten/Lernpfad/images
+cp -r ./de/src/images/*.jpg LernOS-Zettelkasten/Lernpfad/images
+cp -r ./de/src/images/*.png LernOS-Zettelkasten/Lernpfad/images
 
 
 # Create a ZIP archive
-zip -r LernOS-Zettelkasten.zip LernOS-Zettelkasten
+zip -r ./de/LernOS-Zettelkasten.zip LernOS-Zettelkasten
